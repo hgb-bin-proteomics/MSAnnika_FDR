25d3e2dfb4c6c809919780797b255d03e0eceab4
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-01-11 12:35:18 +0100

    crosslink file input tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,27 @@
 # https://github.com/michabirklbauer/
 # micha.birklbauer@gmail.com
 
-# test 1
+# crosslink file without fdr should not do anything
 def test1_msannika_fdr():
 
     from msannika_fdr import main
 
     result = main(["DSSO_CSMs.xlsx"])
 
-    assert True
+    assert len(result) == 0
+
+# crosslink file with fdr should validate
+def test2_msannika_fdr():
+
+    from msannika_fdr import main
+
+    result = main(["DSSO_CSMs.xlsx", "-fdr", "0.01"])
+
+    # check nr of results
+    assert len(result) == 1
+
+    # check nr of crosslinks
+    assert result[0].shape[0] == 714
+
+    # check fdr
+    assert result[0][result[0]["Decoy"] == False].shape[0] / result[0][result[0]["Decoy"] == True].shape[0] < 0.01
